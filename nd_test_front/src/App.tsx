f1cf9717541618bd65283fc7a3268c8ee30a9b4f
Tidiane <tidianecs20pgmail.com>
2026-02-05 19:12:04 +0000

    Init front

---
+++ nd_test_front/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import StComponent from './firstCustomComponent'
+
+function App() {
+  return (
+    <div>
+      <StComponent></StComponent>
+    </div>
+  )
+}
+
+export default App
---
+++ nd_test_front/src/firstCustomComponent.tsx
@@ -0,0 +1,7 @@
+//JUST TO LEARN HOW TO MAKE CUSTOM COMPONENT
+
+function StComponent(){
+    return <h1>Tidiane</h1>
+}
+
+export default StComponent;
